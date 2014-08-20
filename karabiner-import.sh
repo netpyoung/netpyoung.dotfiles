02caece2caf891b8cb9444a053112ca4bfa0e6cd
netpyoung <netpyoung@gmail.com>
2014-08-21 04:55:44 +0900

    .

---
+++ karabiner-import.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.find_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.save_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.pcstyle_close 1
+/bin/echo -n .
+$cli set remap.new_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_new_browser_tab 1
+/bin/echo -n .
+/bin/echo
