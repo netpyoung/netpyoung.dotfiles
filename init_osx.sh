1fe9d5c4781454572114be894132faff806efaac
netpyoung <netpyoung@gmail.com>
2016-06-23 01:02:18 +0900

    init osx

--- init_osx.sh
+++ init_osx.sh
@@ -1,8 +1,7 @@
 GIT_ROOT=`git rev-parse --show-toplevel`
 echo $GIT_ROOT
 
-if [ -f /usr/local/bin/brew ]; then
-else
+if [ ! -f /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
