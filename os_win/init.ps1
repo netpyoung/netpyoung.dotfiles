121147ce8057dd68e1c6992519864158be3334be
netpyoung <netpyoung@gmail.com>
2018-11-04 15:30:28 +0800

    .

--- os_win/init.ps1
+++ os_win/init.ps1
@@ -6,6 +6,35 @@
 
 # scoop : setup
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install git
+scoop install gow
+scoop install ag
+scoop install neovim
+scoop install docker
+scoop install nodejs
 
-# scoop : install program
+# == extra
+scoop bucket add extras
+scoop install emacs
+scoop install baretail
+scoop install everything
+scoop install ConEmu
+scoop install notepadplusplus 
+scoop install spacesniffer
+scoop install RapidEE
+scoop install ccleaner
+scoop install P4Merge
+scoop install cpu-z
+scoop install wireshark
+scoop install winscp
+scoop install putty
+scoop install filezilla
+scoop install sysinternals
+scoop install honeyview 
 
+# == wait....
+# procmon
+# TortoiseGit
+# Astrogrep
+# adobereader
+# Clover
